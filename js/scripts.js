c344811d5e678f2495d8e47a2098ef62375dac83
is.nakolah <nakolahdaniel1@gmail.com>
2019-02-15 16:58:43 +0300

    add all sides to be an array

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,14 @@
 var sideA = parseFloat(document.getElementById("sideA"))
 var sideB = parseFloat(document.getElementById("sideB"))
 var sideC = parseFloat(document.getElementById("sideC"))
+//making all sides to be in an array
 let allSides = [sideA, sideB, sideC]
+//Getting the maximum side
 function maxSide() {
   let max = Math.max(...allSides)
+}
+function triangleType() {
+  if (condition) {
+
+  }
 }
