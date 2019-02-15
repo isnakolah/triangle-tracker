09585ccdced9eb7a89835d6c181f304ad2c28b5a
is.nakolah <nakolahdaniel1@gmail.com>
2019-02-15 16:40:43 +0300

    add the Math.max method

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+//Getting the sides from the HTML
 var sideA = parseFloat(document.getElementById("sideA"))
 var sideB = parseFloat(document.getElementById("sideB"))
 var sideC = parseFloat(document.getElementById("sideC"))
+let allSides = [sideA, sideB, sideC]
+function maxSide() {
+  let max = Math.max(...allSides)
+}
