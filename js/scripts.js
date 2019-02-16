b508c126ea8d81d0e44902aa0f675bdb382d4df0
is.nakolah <nakolahdaniel1@gmail.com>
2019-02-16 23:59:30 +0300

    add identification of a right angled triangle

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,17 @@
 var sideA = parseFloat(document.getElementById("sideA"))
 var sideB = parseFloat(document.getElementById("sideB"))
 var sideC = parseFloat(document.getElementById("sideC"))
+// Getting the data for the functions.
 //making all sides to be in an array.
 let allSides = [sideA, sideB, sideC]
 //Getting the maximum side.
 function maxSide() {
-  let max = Math.max.apply(null, allSides))
+  let max = Math.max.apply(null, allSides)
 }
 //Arranging the sides in ascending order.
-allSides = allSides.sort();
+allSides = allSides.sort((a, b) => a-b );
+smallSides = allSides.slice();
 smallSides.length = 2; //Retains the first two entities of the array.
+console.log(smallSides[0])
 
-function triangleType() {
-  if (condition) {
-
-  }
-}
+//remember to use the += operator
