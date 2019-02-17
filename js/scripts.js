2a2164301c063aea30256a5993fca4f12e49e9a2
Daniel Nakolah <nakolahdaniel1@gmail.com>
2019-02-18 01:01:29 +0300

    add  nan validation rule

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,15 @@ var sideB = parseFloat(document.getElementById("sideB").value);
 var sideC = parseFloat(document.getElementById("sideC").value);
 // Getting the data for the functions.
 //making all sides to be in an array.
+if (sideA > 0 && sideB > 0 && sideC ){
+  var validate = true;
+} else {
+  var validate =false;
+}
+if (validate == false){
+  alert("Please input a valid number.")
+}else runCode();
+function runCode(){
 let allSides = [sideA, sideB, sideC]
 //Getting the maximum side.
   let max = Math.max.apply(null, allSides)
@@ -14,14 +23,14 @@ smallSides = allSides.slice();
 smallSides.length = 2; //Retains the first two entities of the array.
 //remember to use the += operator
 if (Math.pow(smallSides[0], 2)+Math.pow(smallSides[1], 2)=== Math.pow(max, 2)) {
-  alert("The triangle " +sideA + ", "+ sideB+ ", "+ sideC+ " "+"is right angled" )
+  alert("The measurements " +sideA + ", "+ sideB+ ", "+ sideC+ " "+"form a right angled triangle." )
 } else if (smallSides[0]+smallSides[1]<=max) {
   alert("The measurements " +sideA + ", "+ sideB+ ", "+ sideC+ " "+"does not form triangle." )
 } else if (smallSides[0] === smallSides[1] || smallSides[0] === max || smallSides[1] === max)  {
-  alert("The triangle " +sideA + ", "+ sideB+ ", "+ sideC+ " "+"is an isosceles triangle." )
+  alert("The measurements " +sideA + ", "+ sideB+ ", "+ sideC+ " "+"constitute to an isosceles triangle." )
 } else if (smallSides[0] !== smallSides[1] || smallSides[0] !== max || smallSides[1] !== max ) {
-  alert("The triangle " +sideA+ ", "+ sideB+ ", "+ sideC+ " "+"is a scalene triangle." )
+  alert("The measurements " +sideA+ ", "+ sideB+ ", "+ sideC+ " "+"make up a scalene triangle." )
 } else {
-  alert("The triangle "+ sideA+ ", "+ sideB+ ", "+ sideC+ " "+"is an equilateral triangle." )
+  alert("The measurements "+ sideA+ ", "+ sideB+ ", "+ sideC+ " "+"form an equilateral triangle." )
 }
-}
+}}
