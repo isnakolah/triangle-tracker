a511ea4243db5f6173d700c6892fc51edd7f8236
Daniel Nakolah <nakolahdaniel1@gmail.com>
2019-02-18 10:03:06 +0300

    correct the js file

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ function submitIt(){
   // Getting the data for the functions.
   //making all sides to be in an array.
   let validate;
-  if (sideA > 0 && sideB > 0 && sideC ){  
+  if (sideA > 0 && sideB > 0 && sideC > 0 ){  
     validate = true;
   } else {
     validate = false;
@@ -22,7 +22,7 @@ function submitIt(){
     allSides = allSides.sort((a, b) => a-b );
     smallSides = allSides.slice();
     smallSides.length = 2; //Retains the first two entities of the array.
-    if (Math.pow(smallSides[0], 2)+Math.pow(smallSides[1], 2)===      Math.pow(max, 2)) {
+    if (Math.pow(smallSides[0], 2)+Math.pow(smallSides[1], 2)===Math.pow(max, 2)) {
       alert("The measurements " +sideA + ", "+ sideB+ ", "+ sideC+ " "+"form a right angled triangle."); 
      } else if(smallSides[0]===smallSides[1] && smallSides[1]===max){
       alert("The measurements "+ sideA+ ", "+ sideB+ ", "+ sideC+ " "+"form an equilateral triangle." );
@@ -33,7 +33,7 @@ function submitIt(){
     } else if (smallSides[0] !== smallSides[1] || smallSides[0] !== max ||  smallSides[1] !== max ) {
       alert("The measurements " +sideA+ ", "+ sideB+ ", "+ sideC+ " "+"make up a scalene triangle." );
     } else {
-      alert("The measurements "+ sideA+ ", "+ sideB+ ", "+ sideC+ " "+"form an equilateral triangle." );
+      alert("The measurements "+ sideA+ ", "+ sideB+ ", "+ sideC+ " "+"bring an error, please contact developer for more information." );
     }
   }   
 }
