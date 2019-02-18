de28b964401e673f446c461f7b9ed82031b9b062
Daniel Nakolah <nakolahdaniel1@gmail.com>
2019-02-18 03:32:30 +0300

    correct on the equilateral triangle

--- js/scripts.js
+++ js/scripts.js
@@ -24,9 +24,11 @@ function submitIt(){
     smallSides.length = 2; //Retains the first two entities of the array.
     if (Math.pow(smallSides[0], 2)+Math.pow(smallSides[1], 2)===      Math.pow(max, 2)) {
       alert("The measurements " +sideA + ", "+ sideB+ ", "+ sideC+ " "+"form a right angled triangle."); 
+     } else if(smallSides[0]===smallSides[1] && smallSides[1]===max){
+      alert("The measurements "+ sideA+ ", "+ sideB+ ", "+ sideC+ " "+"form an equilateral triangle." );
      } else if (smallSides[0]+smallSides[1]<=max) {
       alert("The measurements " +sideA + ", "+ sideB+ ", "+ sideC+ " "+"do not form triangle." );
-     } else if (smallSides[0] === smallSides[1] || smallSides[0] === max ||  smallSides[1] === max)  {
+     } else if (smallSides[0] === smallSides[1] && smallSides[0]=== max || smallSides[0] === max ||  smallSides[1] === max)  {
       alert("The measurements " +sideA + ", "+ sideB+ ", "+ sideC+ " "+"constitute to an isosceles triangle." );
     } else if (smallSides[0] !== smallSides[1] || smallSides[0] !== max ||  smallSides[1] !== max ) {
       alert("The measurements " +sideA+ ", "+ sideB+ ", "+ sideC+ " "+"make up a scalene triangle." );
